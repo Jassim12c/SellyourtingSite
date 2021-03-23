abc16f0e35d9a99e85f80bc7e1610af7113acf2c
Jassim Alhatem <duod2003row@gmail.com>
2021-03-23 05:13:44 +0300

    add nav button animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,28 @@ const nav = document.querySelector('.nav-list');
 const mainNav = document.querySelector('.main-nav');
 const displayNoneShow = document.getElementById('display-none');
 const header = document.querySelector('.head-title');
+const mobileNav = document.querySelector('.mobile-show-nav');
 
-let x = document.createElement('button');
-x.innerHTML = "X";
+let navShownButton = document.createElement('button');
+navShownButton.innerHTML = "&#10140";
 
 btn.addEventListener('click', ()=>{
     displayNoneShow.style.display = "block";
     nav.classList.add('animated');
     nav.classList.add('slideInLeft');
     mainNav.style.backgroundColor = "rgb(255, 255, 255, 0.2)";
-    mainNav.replaceChild(x, btn);
+    mainNav.replaceChild(navShownButton, btn);
     header.style.marginLeft = "10px";
     nav.classList.remove('slideOutLeft');
+    navShownButton.classList.add('rotate');
 })
 
-x.addEventListener('click', () => {
+navShownButton.addEventListener('click', () => {
     nav.classList.remove('slideInLeft');
     mainNav.style.background = "none";
-    mainNav.replaceChild(btn, x);
+    mainNav.replaceChild(btn, navShownButton);
     nav.classList.add('animated');
     nav.classList.add('slideOutLeft');
     header.style.marginLeft = "-10px";
+    btn.classList.add('rotateReverse');
 })
