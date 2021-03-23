6c071166a7c2dfac34b11d8945fdced1ab2268c4
Jassim Alhatem <duod2003row@gmail.com>
2021-03-24 02:07:27 +0300

    edit favicon url

--- sellsite/urls.py
+++ sellsite/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib.staticfiles.storage import  staticfiles_storage
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("favicon", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico"))),
     path('', include('sell.urls')),
 ]
