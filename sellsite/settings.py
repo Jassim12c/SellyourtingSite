ab36c42aee61309f76d982e1a72edba3e5f28822
Jassim Alhatem <duod2003row@gmail.com>
2021-03-24 13:41:11 +0300

    edit debug

--- sellsite/settings.py
+++ sellsite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "^301u)!o+chojc-f*0b!0wwzuk=7to(nmze-m*bqqv0+xxe&1^"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
