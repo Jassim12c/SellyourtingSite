e757f0457efabdc8dc9c79b83badabcaa52e9d78
Jassim Alhatem <duod2003row@gmail.com>
2021-03-24 13:37:44 +0300

    edit favicon url

--- sellsite/urls.py
+++ sellsite/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("favicon", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico"))),
+    path("favicon", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico"))),
     path('', include('sell.urls')),
 ]
